6b1f598aaf652c8f62574d39e3a34b10567d5e20
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-09-09 15:41:13 +0000

    gh-pages

---
+++ js/index.js
@@ -0,0 +1,22 @@
+document.querySelector('.marker')
+  .innerText = (new Date()).toLocaleTimeString();
+
+document.querySelector('.get-ajax-html')
+    .addEventListener('click', getHtmlAjax);
+
+function getHtmlAjax() {
+    const XHR_STATE_FINISHED = 4;
+    const HTTP_STATUS_OK = 200;
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XHR_STATE_FINISHED
+            && xhr.status === HTTP_STATUS_OK) {
+               document.querySelector('.html-container')
+                 .innerHTML = xhr.responseText;
+        }
+    }
+
+    const clientName = Math.random() > 0.5 ? 'john' :'alice';
+    xhr.open('GET', `client-data-${clientName}.html`, true);
+    xhr.send();
+}
